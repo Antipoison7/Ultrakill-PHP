b9e3c70c2ab0977b17e5aadc1c5d9d4c45f50ec1
Antipoison7 <orders.connor@gmail.com>
2024-09-03 18:03:39 +1000

    Started viewTimes page

---
+++ Website/viewTimes.php
@@ -0,0 +1,19 @@
+<?php
+    include './resources/helper/headers.php';
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Runs</title>
+
+        <link rel='stylesheet' type='text/css' href='./resources/css/common.css' />
+    </head>
+
+    <body>
+        <?php
+            standardHeader("./index.php", "Level Select");
+        ?>
+    </body>
+</html>
