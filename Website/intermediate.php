083bb89d265c7836f95c852b148754e585fa8f2a
Antipoison7 <orders.connor@gmail.com>
2024-09-07 00:41:21 +1000

    Ported intermediate.php

---
+++ Website/intermediate.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<?php
+    include_once('databaseHelper.php');
+    include_once('levelMaker.php');
+?>
+<?php
+    $Runner = $_POST["Runner"];
+    $Category = $_POST["Category"];
+    $Time = $_POST["timeAchieved"];
+    $Video = $_POST["videoLink"];
+    $Comment = $_POST["runnerComment"];
+    $level = $_POST["level"];
+    $Difficulty = $_POST["Difficulty"];
+    $Exit = $_POST["Exit"];
+?>
+
+<html lang="en">
+        <head>
+               <title>Please Dont Break</title>
+
+               <link rel='stylesheet' type='text/css' href='./resources/css/common.css' />
+        <meta http-equiv='refresh' content='5'; url ='index.php'/>
+        </head>
+
+        
+    <body onload='redirectScript()'>
+
+        <p><a href="index.php">Damn, if you see this and it doesn't load, click this. Do not refresh the page.</a></p>
+        <?php
+            echo(var_dump($_POST));
+
+            if(strlen($Video) != 0) //Has a video
+            {
+                if(strlen($Comment) != 0) //Has comment
+                {
+                    addARun($Runner, $Category, toSeconds($Time), $Video, $Comment, $level, $Difficulty, $Exit, 1);
+                }
+                else //Has no Comment
+                {
+                    addARun($Runner, $Category, toSeconds($Time), $Video, $Comment, $level, $Difficulty, $Exit, 2);
+                }
+            }
+            else //Does not have a video
+            {
+                if(strlen($Comment) != 0) //Has comment
+                {
+                    addARun($Runner, $Category, toSeconds($Time), $Video, $Comment, $level, $Difficulty, $Exit, 3);
+                }
+                else //Has no Comment
+                {
+                    addARun($Runner, $Category, toSeconds($Time), $Video, $Comment, $level, $Difficulty, $Exit, 4);
+                }
+            }
+        ?>
+            <script>
+                function redirectScript()
+                {
+                    sleep(1000);
+                    window.location.replace("index.php");
+                }
+                function sleep(ms) {
+                    return new Promise(resolve => setTimeout(resolve, ms));
+                }
+            </script>
+
+    </body>
+</html>
