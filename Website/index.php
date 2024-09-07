15ace25bdcaeaab9c7d5ffce807a4a22336353b3
Antipoison7 <orders.connor@gmail.com>
2024-09-07 16:44:02 +1000

    Fixed Index page links

--- Website/index.php
+++ Website/index.php
@@ -13,7 +13,7 @@
         <div class="flexBox" style='align-items:center;flex-direction:column;'>
             <a href="./viewTimes.php" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='times'><h2 id='timesText'>View Times</h2></div></a>
             <a href="./advTimes.html?level=all" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='advtimes'><h2 id='advtimesText'>Advanced Times</h2></div></a>
-            <a href="./addTimes.html" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='addTimes'><h2 id='addTimesText'>Add Times</h2></div></a>
+            <a href="./addTimes.php" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='addTimes'><h2 id='addTimesText'>Add Times</h2></div></a>
             <a href="./runners.html" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='runners'><h2 id='settingsText'>Runners</h2></div></a>
             <a href="https://www.speedrun.com/ultrakill" class='titleAnchor' target='_blank'><div class='ultrakillTitleButtonMain' id='speedrun'><h2 id='speedText'>Speedrun.com</h2></div></a>
         </div>
