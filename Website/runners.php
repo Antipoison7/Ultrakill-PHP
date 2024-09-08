6b85ebc98243f08ecfcd467e2e06016ce87c62ef
Antipoison7 <orders.connor@gmail.com>
2024-09-09 08:32:43 +1000

    Updated header in runners.php

--- Website/runners.php
+++ Website/runners.php
@@ -13,7 +13,7 @@
     </head>
     <body>
         <?php
-            standardHeader("./index.php", "Runners");
+            sideButtonHeader("./index.php", "Runners", "./addRunner.php");
         ?>
 
         <div class="playersLayout">
