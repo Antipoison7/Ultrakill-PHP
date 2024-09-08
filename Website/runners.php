1cf64f758a149ff5ad501d93031b3285c4280de3
Antipoison7 <orders.connor@gmail.com>
2024-09-09 08:25:14 +1000

    Creaded runners.php

---
+++ Website/runners.php
@@ -0,0 +1,30 @@
+<?php
+    include_once 'headers.php';
+    include_once 'databaseHelper.php';
+    include_once 'levelMaker.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Runner Lookup</title>
+
+        <link rel='stylesheet' type='text/css' href='./resources/css/common.css' />
+    </head>
+    <body>
+        <?php
+            standardHeader("./index.php", "Runners");
+        ?>
+
+        <div class="playersLayout">
+        <?php
+            $runnerCount = getNumberOfRunners();
+            for($i = 0; $i < $runnerCount; $i++)
+            {
+                echo(runnersDisplayGrid($i+1));
+            }
+        ?>
+        </div>
+    </body>
+</html>
+<!-- TODO: Add Custom Header Method -->
