c695193af045940d0132cdbf56e2c801f6abf535
Antipoison7 <orders.connor@gmail.com>
2024-09-09 08:24:19 +1000

    Fixed Typo in headers.php

--- Website/headers.php
+++ Website/headers.php
@@ -3,7 +3,7 @@ function standardHeader($link, $title)
 {
     echo("<div class=\"flexBox\" style=\"justify-content:space-between; margin-bottom:10px;\">");
     echo("<a href=\"" . $link . "\"><button type=\"button\" class=\"button\">Back</button></a>");
-    echo("<h1 class=\"ultrakillTitleText\">" . $title . "</h1>");
+    echo("<h1 class=\"ultrakillTitleText\">-- " . $title . " --</h1>");
     echo("<div style=\"width:15vw;height:4vw;padding: 0vw 2vw;\"></div>");
     echo("</div>");
 }
