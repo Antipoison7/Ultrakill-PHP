dbe960970318111b33f1c0f16bd2b85bfbeaa9d8
Antipoison7 <orders.connor@gmail.com>
2024-09-09 13:30:46 +1000

    Created userDisplay.php

---
+++ Website/userDisplay.php
@@ -0,0 +1,16 @@
+<?php
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Homepage</title>
+
+        <link rel='stylesheet' type='text/css' href='./resources/css/common.css' />
+    </head>
+
+    <body>
+
+    </body>
+</html>
