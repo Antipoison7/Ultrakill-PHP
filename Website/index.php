6daa40362f412c5d5b65c7717a0e9f0706a60bae
Antipoison7 <orders.connor@gmail.com>
2024-09-16 12:27:43 +1000

    Updated index Button Text

--- Website/index.php
+++ Website/index.php
@@ -15,7 +15,7 @@
             <a href="./advTimes.html?level=all" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='advtimes'><h2 id='advtimesText'>Advanced Times</h2></div></a>
             <a href="./addTimes.php" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='addTimes'><h2 id='addTimesText'>Add Times</h2></div></a>
             <a href="./runners.php" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='runners'><h2 id='settingsText'>Runners</h2></div></a>
-            <a href="./login.php" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='runners'><h2 id='settingsText'>Login</h2></div></a>
+            <a href="./login.php" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='runners'><h2 id='settingsText'>Management</h2></div></a>
             <a href="https://www.speedrun.com/ultrakill" class='titleAnchor' target='_blank'><div class='ultrakillTitleButtonMain' id='speedrun'><h2 id='speedText'>Speedrun.com</h2></div></a>
         </div>
         <p class="indexFooterCopyright">Copyright Connor Orders 2024, Ultrakill owned by Hakita. <br>This website uses JavaScript but is gluten free (Does not use cookies).</p>
