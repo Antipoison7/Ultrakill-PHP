02e0bb64ba2fc97c9be698f6520a424b9242184a
Antipoison7 <orders.connor@gmail.com>
2024-09-16 18:39:37 +1000

    Updated Index Links
    Removed login link from index
    Updated advanced times link

--- Website/index.php
+++ Website/index.php
@@ -12,10 +12,9 @@
         <h1 class="ultrakillTitleText">-- EARLY ACCESS --</h1>
         <div class="flexBox" style='align-items:center;flex-direction:column;'>
             <a href="./viewTimes.php" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='times'><h2 id='timesText'>View Times</h2></div></a>
-            <a href="./advTimes.html?level=all" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='advtimes'><h2 id='advtimesText'>Advanced Times</h2></div></a>
+            <a href="./advTimes.php?level=all" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='advtimes'><h2 id='advtimesText'>Advanced Times</h2></div></a>
             <a href="./addTimes.php" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='addTimes'><h2 id='addTimesText'>Add Times</h2></div></a>
             <a href="./runners.php" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='runners'><h2 id='settingsText'>Runners</h2></div></a>
-            <a href="./login.php" class='titleAnchor'><div class='ultrakillTitleButtonMain' id='runners'><h2 id='settingsText'>Management</h2></div></a>
             <a href="https://www.speedrun.com/ultrakill" class='titleAnchor' target='_blank'><div class='ultrakillTitleButtonMain' id='speedrun'><h2 id='speedText'>Speedrun.com</h2></div></a>
         </div>
         <p class="indexFooterCopyright">Copyright Connor Orders 2024, Ultrakill owned by Hakita. <br>This website uses JavaScript.</p>
