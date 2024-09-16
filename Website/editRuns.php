2a7a09685d657d306efdbeb21ced674e33a9f630
Antipoison7 <orders.connor@gmail.com>
2024-09-16 13:05:58 +1000

    Added Children of userManagement

---
+++ Website/editRuns.php
@@ -0,0 +1,49 @@
+<?php
+    session_start();
+    include_once 'headers.php';
+    include_once 'databaseHelper.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Edit Runs</title>
+
+        <style>
+            a
+            {
+                color: white;
+            }
+            
+            a:hover
+            {
+                color:lime;
+            }
+        </style>
+
+        <link rel='stylesheet' type='text/css' href='./resources/css/common.css' />
+    </head>
+
+    <body>
+        <?php
+            standardHeader("./userManagement.php", "EDIT RUNS");
+
+            if(isValidLogin($_SESSION["username"], $_SESSION["password"]))
+            {
+                echo("<p>Dev Stuff</p>");
+            }
+            else
+            {
+                echo("<a href = \"./login.php\">INVALID LOGIN, TRY AGAIN</a>");
+            }
+        ?>        
+    </body>
+</html>
+
+
+<!-- 
+if(($_SESSION["username"] == "Antipoison")&&(isValidLogin($_SESSION["username"], $_SESSION["password"]) == true))
+                {
+                    // echo("admin");
+                } -->
