9d9f99a1810f25b359b205cacb0d0d4f20607def
Antipoison7 <orders.connor@gmail.com>
2024-09-23 08:59:22 +1000

    Updated html cleaner

--- Website/cleaner.php
+++ Website/cleaner.php
@@ -7,8 +7,11 @@
 
     function sanitizeHTML($textIn)
     {
+        $textIn = str_replace("&", "&amp;", $textIn);
         $textIn = str_replace("<", "&lt;", $textIn);
         $textIn = str_replace(">", "&gt;", $textIn);
+        $textIn = str_replace("'", "&#039;", $textIn);
+        $textIn = str_replace('"', "&quot;", $textIn);
         return($textIn);
     }
 ?>
